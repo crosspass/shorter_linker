6f38d146e34dff945f0b89b98ac6ee3dccd21e47
iiithinking <wuyingchao1@gmail.com>
2021-03-18 10:38:27 +0800

    add bootstrap icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def short_url(path)
     "#{request.base_url}/#{path}"
   end
+
+  def bi_icon(icon, options = {})
+    klasses = ['bi'].append(options.delete(:class)).compact
+    content_tag :svg, options.merge(class: klasses, fill: 'currentColor') do
+      content_tag :use, nil, 'xlink:href' => "#{asset_path 'bootstrap-icons/bootstrap-icons.svg'}##{icon}"
+    end
+  end
 end
